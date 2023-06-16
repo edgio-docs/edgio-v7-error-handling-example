fa3952ba54c13c27b75cdfc1e0eb99e56cc79c8c
Mark Brocato <mbrocato@edg.io>
2023-06-16 15:01:56 +0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+const { withEdgio } = require("@edgio/next/config");
+
+module.exports = (phase, config) => withEdgio({});
